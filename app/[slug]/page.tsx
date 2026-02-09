416635b9b53a62dbc9dabae528e7037f6db74106
kshetritej <tejghartikshetri@gmail.com>
2026-02-09 10:49:46 +0545

    add: not found and error page in slug

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,11 +1,18 @@
 import TrekDetailPage from "@/components/trek-detail/TrekDetailPage";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
-  console.log("Slug: ", slug)
+  const { slug } = params;
+
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/activity/slug/${slug}`,
+    { cache: "no-store" },
   );
+
+  if (!res.ok) {
+    notFound();
+  }
+
   const data = await res.json();
 
   const trip = data?.data;
